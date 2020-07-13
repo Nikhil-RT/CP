3267713933a86c48f869237fee9b4086bdd14c1d
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-13 21:32:10 +0530

    Jul 13  21:32:10

--- 01-pythonbasics-Python/test_solution.py
+++ 01-pythonbasics-Python/test_solution.py
@@ -3,6 +3,7 @@ sys.path.append(os.getcwd())
 from pythonbasics import show_excitement
 import pytest
 
+
 result = "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! "
 @pytest.mark.parametrize('x,result',[
     (2, result)
