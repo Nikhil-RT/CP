bac76529750af8edddc5535b9913a0cbbc89d245
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-14 21:30:35 +0530

    Jul 14  21:30:35

--- 09-nearestbusstop-Java/nearestbusstop.java
+++ 09-nearestbusstop-Java/nearestbusstop.java
@@ -17,6 +17,6 @@ class nearestbusstop {
 	public static void main(String[] args) {
 
 		nearestbusstop n = new nearestbusstop();
-		System.out.println(n.fun_nearestbusstop(8));
+		System.out.println(n.fun_nearestbusstop(12));
 	}
 }
