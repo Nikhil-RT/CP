47c1957fe51beacff48616a81f3907c9e9ff0d64
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-14 21:43:51 +0530

    Jul 14  21:43:51

--- 09-nearestbusstop-Java/nearestbusstop.java
+++ 09-nearestbusstop-Java/nearestbusstop.java
@@ -10,6 +10,7 @@ class nearestbusstop {
 		// your code goes here
 		if (street < 0) return -1;
 		double result = street/8;
+		System.out.println(result);
 		return (int)(Math.round(result) * 8);
 
 	}
