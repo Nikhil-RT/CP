6dad95ca2a9ee786af9a96a9f1e635c10af76cb7
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-15 00:44:11 +0530

    Jul 15  00:44:11

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -4,8 +4,34 @@
 # crashing if the value is not an integer. So, isevenpositiveint("yikes!") returns False (rather
 # than crashing), and isevenpositiveint(123456) returns True.
 
+# import pytest
+# import os
+# import sys
+# sys.path.append(os.getcwd())
 def isevenpositiveint(x):
 	# your code goes here
-	if (int(x)>0):
-		return True
-	pass
+	# type(x)
+	if (isinstance(x,int) or x ==(12)):
+		if (x>0 and x%2 ==0):
+			return True
+	return False
+
+# if __name__ == '__main__':
+    	
+# 	@pytest.mark.parametrize('x, check', [
+#     	(1, False),
+#     	(-1, False),
+#     	(-2, False),
+#     	(-3, False),
+#     	(2, True),
+#     	(3, False),
+#     	(1.0, False),
+#     	("yikes!", False),
+#     	(None, False),
+#     	((12), False),
+#     	([12], False),
+#     	(123456, True)
+# 	])
+# 	def test_isevenpositiveint(x, check):
+#     		assert isevenpositiveint(x) == check
+# 	print(isevenpositiveint([12]))
