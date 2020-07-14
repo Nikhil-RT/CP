309a1936e4bfb73a98b05abc23bc63d2f11b6969
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-14 22:36:39 +0530

    Jul 14  22:36:38

--- 09-nearestbusstop-Java/nearestbusstop.java
+++ 09-nearestbusstop-Java/nearestbusstop.java
@@ -10,7 +10,11 @@ class nearestbusstop {
 		// your code goes here
 		if (street < 0) return -1;
 		double result = (double)street/8;
-		result = Math.floor(result);
+		if(street%8 <= 4){
+			result = Math.floor(result);
+		}else{
+			result = Math.floor(result);
+		}
 		result = result * 8;
 		return (int)result;
 
