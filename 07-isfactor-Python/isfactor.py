66b45833b0ed33e649661d19904160a15b6bc9b7
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-14 18:18:24 +0530

    Jul 14  18:18:24

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -7,9 +7,7 @@
 
 def fun_isfactor(f, n):
 	# return False # replace with your solution
-	if (n%f == 0 or n == 0 and f == 0):
+	if (n%f == 0):
 		return True
-	# if (n == 0 or f == 0):
-	# 	return True
 	else:
 		return False	
