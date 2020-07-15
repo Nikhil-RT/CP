088d07051c7a171157e638d2c62ffad9dd8442ad
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-16 00:00:37 +0530

    Jul 16  00:00:37

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,15 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	# return 0
+	mul = -1 if n<0 else 1
+	n = list(str(abs(n)))
+	n = n[::-1]
+	# mul = 
+	# if(n%10 == 0):
+	if (len(n)<=k):
+    		n.append(str(d))
+	n[k] = str(d)
+	n = int("".join(n[::-1]))*mul
+	return n
 
