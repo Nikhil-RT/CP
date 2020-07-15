36bcfaa85d03c74bf700941c0d8d2f2f30738f42
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-15 19:11:01 +0530

    Jul 15  19:11:01

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -1,5 +1,17 @@
 class recursion {
 	public int get_fib(int value){
-	    return -1; 
+		// return -1;
+		if(value == 0) {
+			return 0;
+		}
+		else if( value == 1){
+			return 1;
+		}
+		else {
+			return (get_fib(value - 2)+ get_fib(value-1));
+		}
+	}
+	public static void main(String[] args) {
+		
 	}
 }
