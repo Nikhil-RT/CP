4917151be619222d7c2b50f0c07a746a9391fde0
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-15 21:47:32 +0530

    Jul 15  21:47:32

--- 09-isperfectsquare-Java/IsPerfectSquare.java
+++ 09-isperfectsquare-Java/IsPerfectSquare.java
@@ -7,7 +7,7 @@ import java.lang.Math;
 public class IsPerfectSquare {
 	public boolean isPerfectSquare(int n) {
 		// Your code goes here
-		if (n > 0 && n == (int)Math.sqrt(n*n) ) {
+		if (n >= 0 && n == (int)Math.sqrt(n*n) ) {
 			return true;
 		}
 		return false;
