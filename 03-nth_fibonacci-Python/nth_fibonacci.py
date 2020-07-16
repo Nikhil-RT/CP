802c148c43b85d2904cc53ed7ff9b095e7f1b7e3
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-16 13:28:33 +0530

    Manual commit

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -11,6 +11,8 @@ def fun_nthfibonaccinumber(n):
 	# your code goes Here
 	if (n == 0 or n == 1):
     		return 1
+	elif (n == 2):
+    		return 2
 	else:
 			return fun_nthfibonaccinumber(n-1)+fun_nthfibonaccinumber(n-2)
 	return 0
