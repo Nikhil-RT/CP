62e26eed06cacba60a1fa4563e6bab591a31ad1d
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-16 12:04:26 +0530

    Jul 16  12:04:26

--- 01-find_int_roots-Java/find_int_roots.java
+++ 01-find_int_roots-Java/find_int_roots.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // # Write the function bonusFindIntRoots(a,b,c) that takes 
 // # the int coefficients a, b, c of a quadratic equation of this form:
 // #      y = ax2 + bx + c
@@ -12,10 +14,11 @@ class find_int_roots {
 		// your code goes here
 		
 		int[] arr = {a,b,c};
-		double root1 = ((Math.sqrt(Math.pow(b, 2)-4*a*c)/2*a));
-		double root2 = ((Math.sqrt(Math.pow(b, 2)-4*a*c)/2*a));
+		double root1 = -b-((Math.sqrt(Math.pow(b, 2)-4*a*c)/2*a));
+		double root2 = -b+((Math.sqrt(Math.pow(b, 2)-4*a*c)/2*a));
 		
 		int root[] = {(int)root1,(int)root2};
+		Arrays.sort(root);
 		return root;
 			
 	}
