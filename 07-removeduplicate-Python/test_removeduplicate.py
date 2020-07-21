18c872367c31f5a93c379e5b08107f731998b073
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-21 18:32:34 +0530

    Jul 21  18:32:34

--- 07-removeduplicate-Python/test_removeduplicate.py
+++ 07-removeduplicate-Python/test_removeduplicate.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.mark.parametrize('a, result',[
-    ("JavaPython", "JavPython"), ("HelloWorld", "HeloWrld"), ("EEE", "E"),#Testcase fault for Hello world('l' repeating two times)
+    ("JavaPython", "JavPython"), ("HelloWorld", "HeloWrd"), ("EEE", "E"),#Testcase fault for Hello world('l' repeating two times; so i changed it as single l in the output)
     ("a a ", "a "), ("121212121", "12"), ("", ""),
     ("Test", "Test"), ("1001", "10"), ("11110000", "10"),
 ])
