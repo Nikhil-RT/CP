cb180409374b3ca5ea7ddc644a1b3787df2d5206
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-21 14:23:05 +0530

    Jul 21  14:23:05

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -25,7 +25,7 @@ def ishappynumber(n):
 def fun_nth_happy_prime(n):
     # return 0
     a = []
-    for i in range(len(a)):
+    for i in range(50):
         if (ishappynumber(i)):
             a.append(i)
     return a[n]
