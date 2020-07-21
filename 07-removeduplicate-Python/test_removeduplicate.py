326cd13531ba8db6a126fd41edab7e9184a8873f
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-21 18:31:58 +0530

    Jul 21  18:31:58

--- 07-removeduplicate-Python/test_removeduplicate.py
+++ 07-removeduplicate-Python/test_removeduplicate.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.mark.parametrize('a, result',[
-    ("JavaPython", "JavPython"), ("HelloWorld", "HelloWrld"), ("EEE", "E"),
+    ("JavaPython", "JavPython"), ("HelloWorld", "HeloWrld"), ("EEE", "E"),#Testcase fault for Hello world('l' repeating two times)
     ("a a ", "a "), ("121212121", "12"), ("", ""),
     ("Test", "Test"), ("1001", "10"), ("11110000", "10"),
 ])
