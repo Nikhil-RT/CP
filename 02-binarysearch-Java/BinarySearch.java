4448be0469bdfac11d5b49bbbc0ff065815a4514
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-24 17:08:53 +0530

    Jul 24  17:08:53

--- 02-binarysearch-Java/BinarySearch.java
+++ 02-binarysearch-Java/BinarySearch.java
@@ -1,6 +1,18 @@
 class BinarySearch {
 	public int binary_search(int[] arr, int value){
         // Your code goes here
+        int min = 0;
+        int max = arr.length-1;
+        while (min <= max) {
+                int mid = min + (max - min)/2;
+                if (value < arr[mid]) {
+                        max = mid - 1;
+                }
+                else if (value > arr[mid]) {
+                        min = mid + 1;
+                }
+                else {return mid;}
+        }
         return -1;
 	}
 }
