fbfb6db01484ecbde53f8769e577e979e2ea92f9
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-29 20:33:03 +0530

    Jul 29  20:33:03

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -4,32 +4,28 @@
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
 
-l = [0,1,2,3,4,5,6,7,8,9]
-L = []
-
-def isproperty309():
-	# Your code goes here
-	cnt = 0
-	i = 0
-	n = n**5
-	t = n
-	while n > 0:
-		if i != n%10:
-			n = n//10
-
-		elif i == n%10:
-			cnt = cnt + 1
-			n = t
-			i = i + 1
-			if cnt > 9:
-				return True
-	return False
-
 def nthwithproperty309(n):
-	i = -1
-	g = 0
-	while i <n:
-		g += 1
-		if isproperty309(g):
-			i = i + 1
-	return g
+	def isproperty309(n):
+	# Your code goes here
+		cnt = 0
+		i = 0
+		n = n**5
+		t = n
+		while n > 0:
+			if i != n%10:
+				n = n//10
+
+			elif i == n%10:
+				cnt = cnt + 1
+				n = t
+				i = i + 1
+				if cnt > 9:
+					return True
+		return False
+		i = -1
+		g = 0
+		while i <n:
+			g += 1
+			if isproperty309(g):
+				i = i + 1
+		return g
