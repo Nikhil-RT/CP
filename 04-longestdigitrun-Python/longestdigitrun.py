8335f8c22514a946d339852039f712f1ef17a36a
Nikhil-RT <nraviteja403@msitprogram.net>
2020-07-29 20:11:14 +0530

    Jul 29  20:11:14

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -6,4 +6,26 @@
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
 	# Your code goes here
+	n = abs(n)
+	c = 1
+	lst = []
+	while n > 0:
+		temp = n%10
+		n = n//10
+		if n%10 == temp:
+			c+=1
+		else:
+			lst.append((temp,c))
+			c = 1
+	lst.sort(key = lambda x:x[1],reverse = True)
+	for i in range(len(lst)):
+		if i == len(lst)-1:
+			return lst[i][0]
+		if lst[i][1] == lst[i+1][1]:
+			return lst[i][0]
+		elif lst[i][1] == lst[i+1][0]:
+			if lst[i][0] >= lst[i+1][0]:
+				continue
+			else:
+    				return lst[i][0]
 	pass
