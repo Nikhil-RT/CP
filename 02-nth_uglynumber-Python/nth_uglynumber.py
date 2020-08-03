c7123e9d232044cd0013b6e99c8f7fddc41b73e2
Nikhil-RT <nraviteja403@msitprogram.net>
2020-08-03 14:58:04 +0530

    Aug 03  14:58:04

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -8,7 +8,7 @@ def prime_factors(n):
     while n%2 == 0:
         lst.append(2)
         n //= 2
-    for i in range(3,n,2):
+    for i in range(3,m,2):
         while n%i == 0:
             lst.append(i)
             n //= i
